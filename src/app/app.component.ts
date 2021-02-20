ecfad341760c9d509f7111422ea2565c63baae4e
twolak88 <twolak88@gmail.com>
2021-02-20 22:04:03 +0100

    Advanced Transitions/ wildcard char

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,42 @@ import { Component } from '@angular/core';
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highligted', animate(300)),
+      // transition('highligted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highligted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(0.5)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
       transition('normal => highligted', animate(300)),
-      transition('highligted => normal', animate(800))
+      transition('highligted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state = (this.state === 'normal' ? 'highligted' : 'normal');
+    this.wildState = (this.wildState === 'normal' ? 'highligted' : 'normal');
+  }
+
+  onShrink() {
+    console.log('aAAAAAAAAAAAAAa')
+    this.wildState = 'shrunken';
+    console.log(this.wildState)
   }
 
   onAdd(item) {
