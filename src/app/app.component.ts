e859df5b6b53d990c39bc981fded428d83789762
twolak88 <twolak88@gmail.com>
2021-02-20 21:31:25 +0100

    Animations Triggers and State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highligted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
     this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
   }
 }
